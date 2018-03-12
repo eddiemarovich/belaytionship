4ad3a8de6688cddab2f23f999d7daafef3bbced0
EDDIE MAROVICH <eddie.marovich@gmail.com>
2018-03-12 17:41:50 -0600

    firebase database set up and making calls to api to receive seeded user info

--- belaytionship-app/App.js
+++ belaytionship-app/App.js
@@ -1,17 +1,56 @@
+import Exponent from 'expo'
 import React, {Component} from 'react'
 import {  StyleSheet, View, Image, Text, PanResponder, Animated } from 'react-native'
 import Card from './Card'
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8S2JPfKIGhxV0iQBQdZcTcovOdiyRZK0",
+  databaseURL: "https://belaytionship.firebaseio.com",
+}
+
+firebase.initializeApp(firebaseConfig)
 
 class App extends Component {
 
+  state = {
+    profileIndex: 0,
+    profiles: [],
+  }
+
+  componentWillMount() {
+    firebase.database().ref().child('users').once('value', (data) => {
+      let profiles = []
+      data.forEach((prof) => {
+        const { name, bio, id} = prof.val()
+        profiles.push({name, bio, id})
+      })
+      this.setState({profiles})
+    })
+  }
+
+  nextCard = () => {
+    this.setState({profileIndex: this.state.profileIndex + 1})
+  }
+
   render() {
+    const { profileIndex } = this.state
     return (
       <View style= {{flex: 1}}>
-        <Card />
-        <Card />
+        {this.state.profiles.slice(profileIndex, profileIndex + 3).reverse().map((e, i) => {
+          return (
+            <Card
+              key= {e.id}
+              profile= {e}
+              onSwipeOff= {this.nextCard}
+            />
+          )
+        })}
       </View>
     )
   }
 }
+
+
 
 export default App
